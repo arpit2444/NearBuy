dfd8d87c2fac9b92be7a96028fbe26cda3119c1b
prateek-bka <prateek.bka@gmail.com>
2023-01-19 23:42:02 +0530

    prateek_added_day-3_checkout

--- nearbuy/src/Components/AllRoutes/AllRoutes.jsx
+++ nearbuy/src/Components/AllRoutes/AllRoutes.jsx
@@ -1,15 +1,16 @@
-import { Route, Routes } from "react-router-dom"
-import { Home } from "../../Pages/Home/Home"
+import { Route, Routes } from "react-router-dom";
+import Checkout from "../../Pages/Checkout/Checkout";
+import { Home } from "../../Pages/Home/Home";
 
 export const AllRoutes = () => {
-    return(
-        <Routes>
-            <Route path="/" element={<Home/>}></Route>
-            {/* <Route path="" element={}></Route> */}
-            {/* <Route path="" element={}></Route> */}
-            {/* <Route path="" element={}></Route> */}
-            {/* <Route path="" element={}></Route> */}
-            {/* <Route path="" element={}></Route> */}
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      {/* <Route path="" element={}></Route> */}
+      {/* <Route path="" element={}></Route> */}
+      {/* <Route path="" element={}></Route> */}
+      {/* <Route path="" element={}></Route> */}
+      <Route path="/checkout" element={<Checkout />} />
+    </Routes>
+  );
+};

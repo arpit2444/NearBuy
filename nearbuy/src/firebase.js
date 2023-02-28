c0955395e338e3d84ea141d63dd7ccad39f2cce6
Kashif-Rezwi <kashifrezwi850@email.com>
2023-02-28 14:04:28 +0530

    3 issues are resolved and code is updated

--- nearbuy/src/firebase.js
+++ nearbuy/src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWpGmdh6pw7CG40Y9qs6s14ZlNpGmyfXQ",
-  authDomain: "herebuy-phone-auth.firebaseapp.com",
-  projectId: "herebuy-phone-auth",
-  storageBucket: "herebuy-phone-auth.appspot.com",
-  messagingSenderId: "186736113943",
-  appId: "1:186736113943:web:42ccb0efb3c0eef5919f52"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
